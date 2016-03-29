0ac50f019d4559549c3832d879d5c0210da25c66
MCMLXXX <rfalodiya@outlook.com>
2016-03-28 18:03:36 -0700

    Formatting
    Formatted query and included comment

--- Queries/Advanced_Data_Filtering/Advance Data Filtering_nested And.sql
+++ Queries/Advanced_Data_Filtering/Advance Data Filtering_nested And.sql
@@ -1,1 +1,9 @@
-SELECT Population,Continent,IndepYear From World.Country WHERE (IndepYear = 1971 AND Continent = 'Asia') And Population >617000;
+-- ------------------------------------------------------------------
+-- This query is for demonstrating nesting of one query into another
+SELECT 
+	Population,Continent,IndepYear 
+From 
+	World.Country 
+WHERE 
+	(IndepYear = 1971 AND Continent = 'Asia') And 
+	Population >617000;
