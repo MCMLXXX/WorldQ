320d59e087c8ffdea72794fc328242f5f0a4438d
MCMLXXX <rfalodiya@outlook.com>
2016-04-08 14:04:18 -0700

    explained query

--- Queries/Joining_Tables/Joining_Tables_Aliases.sql
+++ Queries/Joining_Tables/Joining_Tables_Aliases.sql
@@ -1,6 +1,8 @@
 -- ------------------------------------------------------------------
--- -- This query is demonstrating how to join multiple tables 
-
+-- -- This query is demonstrating how to retrieve data from world database about Region and language of a particular district 
+-- -- to retrieve data,joined Country, CountryLanguage and City tables and use aliases to make code simple.
+-- -- Applied district filter om Herat to get data about Herat district.
+ 
 SELECT 
     Region, Language, District
 FROM
