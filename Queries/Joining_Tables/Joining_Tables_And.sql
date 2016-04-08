8c2434ab2f0c0fd4a597b2aea5a666ee8380c650
MCMLXXX <rfalodiya@outlook.com>
2016-04-08 13:52:04 -0700

    commit changes

--- Queries/Joining_Tables/Joining_Tables_And.sql
+++ Queries/Joining_Tables/Joining_Tables_And.sql
@@ -1,11 +1,12 @@
 -- ------------------------------------------------------------------
--- -- This query is demonstrating how to left outer join two tables 
+-- -- This query is demonstrating how to find Region in world database where people speak Dutch language.
+-- -- To execute the Query the tables country and Country language was inner joined and then using filter language = Dutch the region of people speaking Dutch language was retrived--
 
 SELECT 
     Region, Language
 FROM
     World.Country, World.CountryLanguage
-       where
+       WHERE
      World.Country.Code = World.CountryLanguage.Code AND Language = 'Dutch';
      
 -- -------------------------------------------------------------------------
